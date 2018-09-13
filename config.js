ad355d90b8d2a8612d11a4c38e3647cfff337f74
Arjunsoni111 <er.arjunsoni@gmail.com>
2018-09-13 19:20:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  root_folder: '/home/vibrant_node/home/myjournies/arjun/',
-  received_xlsx: 'examples/file2server/received_xlsx/',
-  sent_xlsx: 'examples/file2server/sent_xlsx/',
+  root_folder: '/home/arjun/',
+  received_xlsx: '/received_xlsx/',
+  sent_xlsx: '/sent_xlsx/',
 }
